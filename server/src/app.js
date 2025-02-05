b543251c108c57b2836060945f64257c1849df72
arasruislam <arasru01@gmail.com>
2025-02-05 10:29:14 +0600

    add middleware for CORS and cookie parsing; update app structure and setup instructions

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,28 @@
-// External Imports
+// External Import
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import express from "express";
 
-// create app
+// Create app
 const app = express();
 
-// export app
+// Middleware Config
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// Routes Import
+import userRouter from "./routes/user.routes.js";
+
+// Routes Declaretion
+app.use("/api/v1/users", userRouter);
+
+// Export App
 export { app };
--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,24 @@
 import dotenv from "dotenv";
 
 // Internal Imports
+import { app } from "./app.js";
 import connectDb from "./db/index.js";
 
 // Load environment variables
 dotenv.config({ path: "./.env" });
 
 // Connect to MongoDB
-connectDb();
+connectDb()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error: ", error);
+      throw error;
+    });
+    
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running on PORT: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed ! ", error);
+  });
