8c78c91efdb73a6492324b7d4f227392728fa227
arasruislam <arasru01@gmail.com>
2025-04-21 14:44:07 +0600

    configure app.js

--- express-server/src/index.js
+++ express-server/src/index.js
@@ -12,13 +12,13 @@ connectDB()
          console.log("Error: ", error);
          throw error;
       });
-      
+
       app.listen(process.env.PORT || 3000, () => {
          console.log(`Server is running on port ${process.env.PORT}`);
       });
    })
    .catch((err) => {
-      console.log("MongoDB connection failed!!!", err);
+      console.log("MongoDB connection failed !!! ", err);
    });
 
 /* 
