b6c07d921514fd80cfb00014302204063012e42a
arasruislam <arasru01@gmail.com>
2025-04-21 12:22:32 +0600

    feat(db): enhance MongoDB connection setup and update dev script

--- express-server/src/index.js
+++ express-server/src/index.js
@@ -1,5 +1,11 @@
-import express from "express";
-const app = express();
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+   path: "./env",
+});
+
+connectDB();
 
 /* 
 * first approach with async await
