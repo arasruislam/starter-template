e9da7d6f0f94e2d83876ea006e4f64387e21bc43
arasruislam <arasru01@gmail.com>
2025-04-22 11:33:07 +0600

    fix(controller): update user registration logic and error handling

--- express-server/src/controllers/user.controller.js
+++ express-server/src/controllers/user.controller.js
@@ -1,4 +1,7 @@
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const registerUser = asyncHandler(async (req, res) => {
    // get user details from frontend
@@ -11,11 +14,49 @@ const registerUser = asyncHandler(async (req, res) => {
    // check for user creation
    // send response back to frontend
 
-   const { username, email, fullname, password } = req.body;
+   const { username, email, fullName, password } = req.body;
 
-   
+   if([fullName, email, username, password].some((field) => field?.trim() === "")) {
+      throw new ApiError(400, "Please fill all the fields");
+   }
 
+   const existedUser = await User.findOne({
+      $or: [
+         { username }, { email }
+      ]
+   });
+   if (existedUser) {
+      throw new ApiError(409, "User already exists");
+   }
 
+   const avatarLocalPath = req.files?.avatar[0]?.path;
+   const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+   if(!avatarLocalPath) {
+      throw new ApiError(400, "Please upload an avatar");
+   }
+   const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+  /*
+   * if cover image required then this code will be uncommented
+   if(!coverImageLocalPath) {
+      throw new ApiError(400, "Please upload a cover image");
+   }
+   */
+   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+   if(!avatar) {
+      throw new ApiError(400, "Unable to upload avatar");
+   }
+
+   const user = await User.create({
+      username: username.toLowerCase(),
+      email,
+      fullName,
+      password,
+      avatar: avatar?.url,
+      coverImage: coverImage?.url || ""
+   });
 
 })
 
