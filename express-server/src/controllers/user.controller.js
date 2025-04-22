af0511eeb87b82bcf6a4608859875220b723fd20
arasruislam <arasru01@gmail.com>
2025-04-22 10:28:06 +0600

    feat(routes): add user registration route and controller

--- express-server/src/controllers/user.controller.js
+++ express-server/src/controllers/user.controller.js
@@ -3,3 +3,7 @@ import { asyncHandler } from "../utils/asyncHandler";
 const registerUser = asyncHandler(async (req, res) => {
    res.status(200).json({message: "ok"})
 })
+
+
+
+export { registerUser };
